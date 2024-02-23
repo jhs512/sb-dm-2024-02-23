f7a661b7d1bc7bdae13a9daa4b3263dfc14fabab
jhs512 <jangka512@gmail.com>
2024-02-23 15:44:23 +0900

    work

---
+++ src/main/java/com/ll/sbdm20240223/global/aws/dynamoDb/DynamoDbConfig.java
@@ -0,0 +1,25 @@
+package com.ll.sbdm20240223.global.aws.dynamoDb;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.awscore.defaultsmode.DefaultsMode;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+@Configuration(enforceUniqueMethods = false)
+public class DynamoDbConfig {
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .defaultsMode(DefaultsMode.AUTO)
+                .build();
+    }
+
+    @Bean
+    public DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient dynamoDbClient) {
+        return DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(dynamoDbClient)
+                .build();
+    }
+}
