a9c122cd1e6ac35906aa5ea21d07726c5a93a632
jhs512 <jangka512@gmail.com>
2024-02-26 09:48:56 +0900

    work

---
+++ src/main/java/com/ll/sbdm20240223/domain/chat/chat/entity/DmChatMessage.java
@@ -0,0 +1,30 @@
+package com.ll.sbdm20240223.domain.chat.chat.entity;
+
+
+import lombok.*;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
+
+@DynamoDbBean
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DmChatMessage {
+    private String id;
+    private Long chatRoomId;
+    private String createDate;
+    private String message;
+
+    @DynamoDbPartitionKey
+    public Long getChatRoomId() {
+        return chatRoomId;
+    }
+
+    @DynamoDbSortKey
+    public String getCreateDate() {
+        return createDate;
+    }
+}
